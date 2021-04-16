fa0865e63884972cfcd76fd38a6aaf112ef855d0
maxly20 <maxly2011@gmail.com>
2021-04-16 13:09:34 +1000

    refactor files

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -1,47 +1,43 @@
-import React, {createContext, useState, useEffect} from 'react'
+import React, { createContext, useState, useEffect } from 'react';
 import data from '../assets/data/data';
 
 export const DataContext = createContext();
 
-export const DataProvider = (props) => {
-    const [products, setProducts] = useState(data)
+export const DataProvider = props => {
+  const [products, setProducts] = useState(data);
 
-    const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-    const addCart = (id) =>{
-        const check = cart.every(item =>{
-            return item._id !== id
-        })
-        if(check){
-            const data = products.filter(product =>{
-                return product._id === id
-            })
-            setCart([...cart, ...data])
-        }else{
-            alert("The product has been added to cart.")
-        }
+  const addCart = id => {
+    const check = cart.every(item => {
+      return item._id !== id;
+    });
+    if (check) {
+      const data = products.filter(product => {
+        return product._id === id;
+      });
+      setCart([...cart, ...data]);
+    } else {
+      alert('The product has been added to cart.');
     }
+  };
 
-    useEffect(() =>{
-       const dataCart =  JSON.parse(localStorage.getItem('dataCart'))
-       if(dataCart) setCart(dataCart)
-    },[])
+  useEffect(() => {
+    const dataCart = JSON.parse(localStorage.getItem('dataCart'));
+    if (dataCart) setCart(dataCart);
+  }, []);
 
-    useEffect(() =>{
-        localStorage.setItem('dataCart', JSON.stringify(cart))
-    },[cart])
+  useEffect(() => {
+    localStorage.setItem('dataCart', JSON.stringify(cart));
+  }, [cart]);
 
+  const value = {
+    products: [products, setProducts],
+    cart: [cart, setCart],
+    addCart: addCart,
+  };
 
-    const value = {
-        products: [products, setProducts],
-        cart: [cart, setCart],
-        addCart: addCart
-    }
-
-    
-    return (
-        <DataContext.Provider value={value}>
-            {props.children}
-        </DataContext.Provider>
-    )
-}
+  return (
+    <DataContext.Provider value={value}>{props.children}</DataContext.Provider>
+  );
+};
--- src/components/DetailsThumb.js
+++ src/components/DetailsThumb.js
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export default function DetailsThumb({images, setIndex}) {
-    return (
-        <div className="thumb">
-            {
-                images.map((img, index)=>(
-                    <img src={img} alt="" key={index} 
-                    onClick={() => setIndex(index)} />
-                ))
-            }
-        </div>
-    )
+export default function DetailsThumb({ images, setIndex }) {
+  return (
+    <div className='thumb'>
+      {images.map((img, index) => (
+        <img src={img} alt='' key={index} onClick={() => setIndex(index)} />
+      ))}
+    </div>
+  );
 }
